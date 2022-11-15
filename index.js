5af5dd3e5a221049c35fcfef271e594e012b809a
unknown <sambas6418@gmail.com>
2022-11-15 21:00:26 +0700

    Project chalenge

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send("Server Ok")
+})
+app.listen(3000,()=>{
+    console.log('Running server port:3000')
+})
